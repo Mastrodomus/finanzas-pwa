8c6ef0cdeb537d8111395cdb9e3e99c0d8ef716a
Mastrodomus <santigallastegui@gmail.com>
2026-01-02 08:40:35 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Registrar service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+
+const ing = document.getElementById("ing");
+const egr = document.getElementById("egr");
+const out = document.getElementById("out");
+
+document.getElementById("btn").addEventListener("click", () => {
+  const ingresos = Number(ing.value || 0);
+  const egresos  = Number(egr.value || 0);
+  const fcff = ingresos - egresos;
+
+  out.textContent = JSON.stringify({ fcff }, null, 2);
+});
